b5ef67368841a5619e60670c9bcfdca6e913d5b0
Ch. Muhammad Sohaib <35073175+chmuhammadsohaib@users.noreply.github.com>
2020-07-11 21:20:50 +0000

    Add Support for Different Modes

--- NHXCrypt.py
+++ NHXCrypt.py
@@ -13,14 +13,18 @@ encdec = input("(E)ncrypt/(D)ecrypt: ").lower()
 if encdec not in ("d", "e"):
 	print("Exitting..")
 	exit(-2)
-mode = 64
+mode = input("Enter the mode (8/16/32/64/128): ")
+if mode not in ("8", "16", "32", "64", "128"):
+	print("Error: Invalid Mode")
+	exit(5)
+mode = int(mode)
 key = input("Enter the Passkey:\t")
 if key == "":
 	print("Error: No encryption key provided")
 	exit(-5)
 data = read.read()
 write = open(sys.argv[2], "wb")
-if encdec == "e":	
+if encdec == "e":
 	if len(data) < len(key):
 		tukey = key[0:len(data)]
 	elif len(data) > len(key):
@@ -31,25 +35,39 @@ if encdec == "e":
 		tukey = key
 	parandom = os.urandom(mode * len(data))
 	count = 0
-	for char in range(len(data)):			
-		if ord(tukey[char]) > (mode - 1):
-			stoplength = ord(tukey[char]) - (mode - 1)
+	for char in range(len(data)):
+		tudata = ord(data[char: char + 1])		
+		if mode < 64:
+			if ord(tukey[char]) > (mode - 1):
+				keyval = ord(tukey[char])
+				acc = keyval - mode
+				while acc > (mode - 1):
+					 acc = int(acc) / 2
+				stoplength = int(acc)
+				diff = keyval - int(acc)
+				tudata = tudata + diff
+			else:
+				stoplength = ord(tukey[char])
 		else:
-			stoplength = ord(tukey[char])
-		temp = []
+			if ord(tukey[char]) > (mode - 1):
+				stoplength = ord(tukey[char]) - (mode - 1)
+			else:
+				stoplength = ord(tukey[char])
+		temp = 0
 		for i in range(mode):
-			if len(temp) == stoplength - 1:
-				write.write(data[char: char + 1])
-				temp.append(chr(data[char]))
-			else:		
-				write.write(parandom[(count*mode) + i : (count*mode) + 1 + i])
-				temp.append(parandom[(count*mode) + i])
+			if temp == stoplength - 1:
+				write.write(chr(tudata).encode())
+				temp = temp + 1
+			else:
+				write.write(chr(ord(parandom[(count*mode) + i : (count*mode) + 1 + i])).encode())
+				temp = temp + 1
 		count = count + 1
-	print("Info: The Encrypted text is written to the file specified.")
+	print("Info: The Encrypted data is written to the file specified.")
 elif encdec == "d":
+	data = data.decode()
 	decbytes = int(len(data) / mode)
 	if len(data) % mode != 0:
-		print("Error: The encrypted text is corrupted")
+		print("Error: Incorrect mode or the encrypted file is corrupted")
 		exit(-3)
 	if decbytes < len(key):
 		tukey = key[0:len(data)]
@@ -59,13 +77,32 @@ elif encdec == "d":
 		tukey = tukey[0:len(data)]
 	count = 0	
 	for char in range(decbytes):
-		if ord(tukey[char]) > (mode-1):
-			location = ord(tukey[char]) - (mode - 1)
-		else:		
-			location = ord(tukey[char])
-		write.write(data[location + (mode * count) - 1 : location + (mode * count)])
+		diff = 0
+		if mode < 64:
+			if ord(tukey[char]) > (mode - 1):
+				keyval = ord(tukey[char])
+				acc = keyval - mode
+				while acc > (mode - 1):
+					 acc = int(acc) / 2
+				location = int(acc)
+				diff = keyval - int(acc)
+			else:
+				location = ord(tukey[char])
+		else:
+			if ord(tukey[char]) > (mode-1):
+				location = ord(tukey[char]) - (mode - 1)
+			else:		
+				location = ord(tukey[char])
+		tudata = data[location + (mode * count) - 1 : location + (mode * count)]
+		if diff != 0:
+			try:
+				tudata = chr(ord(tudata) - diff)
+			except ValueError:
+				print("Error: Incorrect Mode/Password caused crash during value altering")
+				exit(404)
+		write.write(tudata.encode())
 		count = count + 1
-	print("Info: The Decrypted text is written to the file specified.")
+	print("Info: The Decrypted data is written to the file specified.")
 read.close()
 write.close()
 
